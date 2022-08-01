b9c8552500b2a0ce05b120f168759fe5b5e7b1fa
Giannis Gkountras <110430201+giannisgkountras@users.noreply.github.com>
2022-08-02 02:58:56 +0300

    Update Encrypt.java

--- Encrypt.java
+++ Encrypt.java
@@ -64,40 +64,5 @@ public class Encrypt {
 		System.out.println("Encoded message is: " + Arrays.toString(encodedMsgInt));
 	}
 	
-	/*public static void main(String[] args) {
-		String encryptMsg , decryptMsg , key;
-		int choice = 0;
-		Scanner scanner = new Scanner(System.in);
-		while (choice != 99){
-			System.out.println("Type 1 and press enter for encryption.");
-			System.out.println("Type 2 and press enter for decryption.");
-			System.out.println("Type 99 and press enter to exit the program.");
-			choice = scanner.nextInt();
-			scanner.nextLine();
-			if (choice == 1) {
-				System.out.println("Type the key you want to use and press enter:");
-				key = scanner.nextLine();
-				System.out.println("Type the message you want to encrypt and press enter:");
-				encryptMsg = scanner.nextLine();
-				Encrypt encrypt = new Encrypt(encryptMsg, key);
-				encrypt.encr();
-				System.out.println("\n\n\n");
-				}
-			else if (choice == 2) {
-				System.out.println("Type the key you want to use and press enter:");
-				key = scanner.nextLine();
-				System.out.println("Type the message you want to decrypt and press enter:");
-				encryptMsg = scanner.nextLine();
-				Decrypt decrypt = new Decrypt(encryptMsg, key);
-				decrypt.decr();
-				}
-			else if (choice == 99){
-				break;
-			}
-			else {
-				System.out.println("You did not choose an option");
-			}
-		}
-		
-	}*/
+	
 }
